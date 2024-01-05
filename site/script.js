665537ec69945529859910fb2d0be25f4203c6fa
nikkou0814 <125991634+nikkou-0814@users.noreply.github.com>
2024-01-06 04:58:28 +0900

    Update site/index.html and site/style.css

--- site/script.js
+++ site/script.js
@@ -5,6 +5,8 @@ var dltext = document.getElementById('download-text');
 var image = document.getElementById('image');
 var toptext1 = document.getElementById('toppage-text1');
 var toptext2 = document.getElementById('toppage-text2');
+var dlbtn = document.querySelector('.dlbtn');
+var sp = document.getElementById('sp');
 var lastScrollTop = 0;
 
 setTimeout(function() {
@@ -19,7 +21,6 @@ setTimeout(function() {
     header.style.borderRadius = '0 0 50px 50px';
     header.style.border = '3px solid rgb(255, 255, 255, 0.2)';
     dl.style.display = 'flex';
-    headerText.style.left = '20px';
     headerText.style.fontSize = '24px';
     setTimeout(function() {
         header.classList.add('bounce');
@@ -28,27 +29,23 @@ setTimeout(function() {
 }, 1500);
 
 setTimeout(function() {
-    dl.style.opacity = 1;
-    dl.style.transform = 'scale(1)';
-    dl.style.filter = 'none';
-}, 2000);
-
-setTimeout(function() {
-    image.style.opacity = 1;
-    image.style.transform = 'scale(1)';
-    image.style.filter = 'none';
-}, 2000);
-
-setTimeout(function() {
     toptext1.style.opacity = 1;
     toptext1.style.transform = 'scale(1)';
     toptext1.style.filter = 'none';
-}, 2500);
+    header.classList.delete('bounce');
+}, 2000);
 
 setTimeout(function() {
     toptext2.style.opacity = 1;
     toptext2.style.transform = 'scale(1)';
     toptext2.style.filter = 'none';
+
+}, 2500);
+
+setTimeout(function() {
+    image.style.opacity = 1;
+    image.style.transform = 'scale(1)';
+    image.style.filter = 'none';
 }, 3000);
 
 window.addEventListener('scroll', function() {
@@ -64,6 +61,8 @@ window.addEventListener('scroll', function() {
         dl.style.height = '30px';
         dl.style.borderRadius = '30px'
         dltext.style.fontSize = '15px';
+        dlbtn.style.height = '15px';
+        dlbtn.style.width = '15px';
     } else {
         // Upscroll
         header.style.height = '60px';
@@ -72,11 +71,31 @@ window.addEventListener('scroll', function() {
         header.style.border = '3px solid rgb(255, 255, 255, 0.2)';
         dl.style.top = '7px';
         dl.style.right = '25px';
-        dl.style.height = '45px';
+        dl.style.height = '40px';
         dl.style.borderRadius = '15px'
         dltext.style.fontSize = '20px';
+        dlbtn.style.height = '20px';
+        dlbtn.style.width = '20px';
     }
     lastScrollTop = scrollTop;
+});
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY === 0) {
+        dl.style.transform = 'scale(0.5)';
+        dl.style.filter = 'blur(30px)';
+        dl.style.opacity = '0';
+        headerText.style.transition = 'all 0.6s cubic-bezier(0.22, 1, 0.36, 1)';
+        headerText.style.left = '50%';
+        headerText.style.transform = 'translateX(-50%)';
+    } else {
+        dl.style.transform = 'scale(1)';
+        dl.style.filter = 'none';
+        dl.style.opacity = '1';
+        headerText.style.left = '20px';
+        headerText.style.transform = 'none';
+    }
+
 });
 
 window.addEventListener('scroll', function() {
